e501090790d7db65f856840b838da094c839e457
sikandar <royalsikandar25@gmail.com>
2024-12-15 00:48:48 +0530

    new change

--- src/Component/Common/Header/Header.js
+++ src/Component/Common/Header/Header.js
@@ -92,95 +92,96 @@ const Header = () => {
 
     const uniqueSuggestions = uniqBy(suggestions, item => searchBy === 'product' ? item.product_name : item.product_store_name);
     return (
-        <>
-            <header className="header">
-                <img src={logocolor} height="40px" width="40px" onClick={() => navigate('/')} />
+        // <>
+        //     <header className="header">
+        //         <img src={logocolor} height="40px" width="40px" onClick={() => navigate('/')} />
 
-                <Select
-                    labelInValue
-                    defaultValue={{
-                        value: 'en',
-                        label: 'select language',
-                    }}
-                    className='select-locale'
-                    onChange={handleChange}
-                    options={[
-                        {
-                            value: 'en',
-                            label: 'En',
-                        },
-                        {
-                            value: 'hi',
-                            label: 'हिंदी',
-                        },
-                    ]}
-                />
-                {search.includes(location.pathname) && <div className="search-container">
-                    <select
-                        className="search-by-dropdown"
-                        value={searchBy}
-                        onChange={(e) => setSearchBy(e.target.value)}
-                    >
-                        <option value="product">Product</option>
-                        <option value="store">Store</option>
-                    </select>
-                    <AutoComplete
-                        className='auto-complete'
-                        onSearch={debounce(handleSearch, 300)}
-                        onSelect={handleSelect}
-                        onKeyDown={SearchByFilter}
-                        options={uniqueSuggestions.map(item => ({
-                            value: searchBy === 'product' ? item.product_name : item.product_store_name,
-                        }))}
-                        placeholder="Search Item"
-                        value={searchValue} // Bind searchValue to AutoComplete
-                    />
-                    {searchValue.length >= 3 && (
-                        <button className='clear-filter' onClick={clearFilter}>
-                            <i className='fa fa-times'></i>
-                        </button>
-                    )}
-                    <button className="search-icon" onClick={SearchByFilter}>
-                        <i className="fa fa-search" aria-hidden="true"></i>
-                    </button>
-                </div>}
-                {cart.length > 0 && <Badge count={cart.length} onClick={redirectHandler} className='notification-badge'>
-                    <i className="fa fa-shopping-cart" aria-hidden="true"></i>
-                </Badge>}
+        //         <Select
+        //             labelInValue
+        //             defaultValue={{
+        //                 value: 'en',
+        //                 label: 'select language',
+        //             }}
+        //             className='select-locale'
+        //             onChange={handleChange}
+        //             options={[
+        //                 {
+        //                     value: 'en',
+        //                     label: 'En',
+        //                 },
+        //                 {
+        //                     value: 'hi',
+        //                     label: 'हिंदी',
+        //                 },
+        //             ]}
+        //         />
+        //         {search.includes(location.pathname) && <div className="search-container">
+        //             <select
+        //                 className="search-by-dropdown"
+        //                 value={searchBy}
+        //                 onChange={(e) => setSearchBy(e.target.value)}
+        //             >
+        //                 <option value="product">Product</option>
+        //                 <option value="store">Store</option>
+        //             </select>
+        //             <AutoComplete
+        //                 className='auto-complete'
+        //                 onSearch={debounce(handleSearch, 300)}
+        //                 onSelect={handleSelect}
+        //                 onKeyDown={SearchByFilter}
+        //                 options={uniqueSuggestions.map(item => ({
+        //                     value: searchBy === 'product' ? item.product_name : item.product_store_name,
+        //                 }))}
+        //                 placeholder="Search Item"
+        //                 value={searchValue} // Bind searchValue to AutoComplete
+        //             />
+        //             {searchValue.length >= 3 && (
+        //                 <button className='clear-filter' onClick={clearFilter}>
+        //                     <i className='fa fa-times'></i>
+        //                 </button>
+        //             )}
+        //             <button className="search-icon" onClick={SearchByFilter}>
+        //                 <i className="fa fa-search" aria-hidden="true"></i>
+        //             </button>
+        //         </div>}
+        //         {cart.length > 0 && <Badge count={cart.length} onClick={redirectHandler} className='notification-badge'>
+        //             <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+        //         </Badge>}
 
-                <i className="fa fa-bars menu-icon" aria-hidden="true" onClick={toggleMenu}></i>
-            </header>
+        //         <i className="fa fa-bars menu-icon" aria-hidden="true" onClick={toggleMenu}></i>
+        //     </header>
 
-            <div className={`sidenav-overlay ${menuVisible ? 'visible' : ''}`} onClick={toggleMenu}>
-                <div className={`sidenav ${menuVisible ? 'open' : ''}`} onClick={e => e.stopPropagation()}>
-                    <a href="javascript:void(0)" className="closebtn" onClick={toggleMenu}>&times;</a>
-                    <div className="sidenav-extra">
-                        <div className="top-left-container">
-                            <div className="logo">
-                                <img src="https://via.placeholder.com/50" alt="Logo" />
-                            </div>
-                            <Badge count={cart.length} className="cart-badge">
-                                <i className="fa fa-shopping-cart" aria-hidden="true"></i>
-                            </Badge>
-                        </div>
-                        <Avatar size={64} icon={<i className="fa fa-user" />} />
-                        <Button type="primary" block onClick={handleLogin}>Login</Button>
-                        <Select defaultValue={i18n.language} style={{ width: '100%' }} onChange={handleLanguageChange}>
-                            <Option value="en">English</Option>
-                            <Option value="es">Español</Option>
-                            <Option value="fr">Français</Option>
-                            <Option value="de">Deutsch</Option>
-                        </Select>
-                        <Select defaultValue="Product" style={{ width: '100%' }} onChange={(value) => setSearchBy(value)}>
-                            <Option value="product">Product</Option>
-                            <Option value="store">Store</Option>
-                        </Select>
-                    </div>
-                </div>
-            </div>
-            <h1>hello</h1>
-            {!noHrscroll.includes(location.pathname) && <HorizontalScroll />}
-        </>
+        //     <div className={`sidenav-overlay ${menuVisible ? 'visible' : ''}`} onClick={toggleMenu}>
+        //         <div className={`sidenav ${menuVisible ? 'open' : ''}`} onClick={e => e.stopPropagation()}>
+        //             <a href="javascript:void(0)" className="closebtn" onClick={toggleMenu}>&times;</a>
+        //             <div className="sidenav-extra">
+        //                 <div className="top-left-container">
+        //                     <div className="logo">
+        //                         <img src="https://via.placeholder.com/50" alt="Logo" />
+        //                     </div>
+        //                     <Badge count={cart.length} className="cart-badge">
+        //                         <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+        //                     </Badge>
+        //                 </div>
+        //                 <Avatar size={64} icon={<i className="fa fa-user" />} />
+        //                 <Button type="primary" block onClick={handleLogin}>Login</Button>
+        //                 <Select defaultValue={i18n.language} style={{ width: '100%' }} onChange={handleLanguageChange}>
+        //                     <Option value="en">English</Option>
+        //                     <Option value="es">Español</Option>
+        //                     <Option value="fr">Français</Option>
+        //                     <Option value="de">Deutsch</Option>
+        //                 </Select>
+        //                 <Select defaultValue="Product" style={{ width: '100%' }} onChange={(value) => setSearchBy(value)}>
+        //                     <Option value="product">Product</Option>
+        //                     <Option value="store">Store</Option>
+        //                 </Select>
+        //             </div>
+        //         </div>
+        //     </div>
+        //     <h1>hello</h1>
+        //     {!noHrscroll.includes(location.pathname) && <HorizontalScroll />}
+        // </>
+        <>hiiiiii</>
     );
 };
 
